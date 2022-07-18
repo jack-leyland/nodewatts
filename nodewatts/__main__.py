2d84b41e01b61c6f83bec4be4b9e5ed8202a5714
jack-leyland <jackjleyland@gmail.com>
2022-07-18 15:31:01 +0100

    module config, db config by uri only now

--- nodewatts/__main__.py
+++ nodewatts/__main__.py
@@ -1,17 +1,54 @@
 import modules.nodewatts_data_engine.nwengine.log as log
+from .error import NodewattsError
 from .config import NWConfig, InvalidConfig
-import os 
+import os
 import sys
 import argparse
 import json
 
-def create_cli_parser():
-    parser = argparse.ArgumentParser(description='NodeWatts: Power profiling tool for NodeJS web servers')
+
+def create_cli_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description='NodeWatts: Power profiling tool for NodeJS web servers')
     parser.add_argument('--config_file', type=str, required=True)
     return parser
 
+
+def validate_module_configs() -> None:
+    sensor_path = os.path.join(
+        os.getcwd(), "./nodewatts/config/hwpc_config.json")
+    sw_path = os.path.join(
+        os.getcwd(), "./nodewatts/config/smartwatts_config.json"
+    )
+    if not os.path.exists(sensor_path):
+        raise NodewattsError(
+            "Sensor config file missing at path: " + sensor_path)
+    if not os.path.exists(sw_path):
+        raise NodewattsError(
+            "Smartwatts config file missing at path: " + sw_path
+        )
+
+    with open(sensor_path, "r") as f:
+        try:
+            sensor_raw = json.load(f)
+        except json.decoder.JSONDecodeError as e:
+            raise InvalidConfig(
+                "Sensor config file must be in valid json format") from None
+    NWConfig.validate_sensor_config(sensor_raw)
+
+    with open(sw_path, "r") as f:
+        try:
+            sw_raw = json.load(f)
+        except json.decoder.JSONDecodeError as e:
+            raise InvalidConfig(
+                "Smartwatts config file must be in valid json format") from None
+    NWConfig.validate_smartwatts_config(sw_raw)
+
+
 def run(config: NWConfig):
-    pass
+    validate_module_configs()
+    config.inject_module_config_vars()
+
 
 if __name__ == "__main__":
     conf = NWConfig()
@@ -21,7 +58,8 @@ if __name__ == "__main__":
     try:
         raw = json.load(open(conf.config_file))
     except json.decoder.JSONDecodeError as e:
-        raise InvalidConfig("Configuration file must be in valid json format") from None
+        raise InvalidConfig(
+            "Configuration file must be in valid json format") from None
     NWConfig.validate(raw)
     conf.setup(raw)
     if not conf.verbose:
@@ -31,4 +69,3 @@ if __name__ == "__main__":
     run(conf)
     logger.info("Profile generated! Exiting...")
     sys.exit(0)
-
--- nodewatts/config.py
+++ nodewatts/config.py
@@ -3,10 +3,13 @@ from modules.nodewatts_data_engine.nwengine.config import Config
 from nodewatts.error import NodewattsError
 import os
 import json
+import jsonschema as jschema
+
 
 class InvalidConfig(NodewattsError):
     def __init__(self, msg: str, *args, **kwargs):
         super().__init__(msg, *args, **kwargs)
+
 
 class NWConfig(Config):
     def __init__(self):
@@ -19,61 +22,229 @@ class NWConfig(Config):
         self.commands = args["commands"]
         self.entry_file = args["entryFile"]
         self.engine_conf_args = self._to_engine_format(args)
-    
+
     @staticmethod
     def validate(args: dict) -> None:
         missing = {}
-        if "rootDirectoryPath" not in args.keys():
+        if "rootDirectoryPath" not in args:
             missing["rootDirectoryPath"] = "[Absolute path to project root]"
-        if "entryFile" not in args.keys():
+        if "entryFile" not in args:
             missing["entryFile"] = "[Name of server entry file]"
-        if "database" not in args.keys():
-            missing["database"] = "[Database configuration object]"
-        if "verbose" not in args.keys():
+        if "database" not in args:
+            missing["database"] = {
+                "uri": "[MongoDB uri for internal nodewatts DB]",
+                "exportRawData": "[Boolean setting for exporting raw nodewatts data]",
+                "exportUri": "[Required if exporting: MongoDB URI to send export]",
+                "exportDbName": "[Required if exporting: Name of database to send export]"
+            }
+        else:
+            if "uri" not in args["database"]:
+                missing["database"] = {
+                    "uri": "[MongoDB uri for internal nodewatts DB]"}
+            if "exportRawData" not in args["database"]:
+                if "database" not in missing:
+                    missing["database"] = {
+                        "exportRawData": "[Boolean setting for exporting raw nodewatts data]"}
+                else:
+                    missing["database"]["exportRawData"] = "[Boolean setting for exporting raw nodewatts data]"
+            if "exportUri" not in args["database"]:
+                if "database" not in missing:
+                    missing["database"] = {
+                        "exportUri": "[Required if exporting: MongoDB URI to send export]"}
+                else:
+                    missing["database"]["exportUri"] = "[Required if exporting: MongoDB URI to send export]"
+            if "exportDbName" not in args["database"]:
+                if "database" not in missing:
+                    missing["database"] = {
+                        "exportDbName": "[Required if exporting: Name of database to send export]"}
+                else:
+                    missing["database"]["exportDbName"] = "[Required if exporting: Name of database to send export]"
+        if "verbose" not in args:
             missing["verbose"] = "[Boolean debug level setting]"
-        if "visualize" not in args.keys():
+        if "visualize" not in args:
             missing["visualize"] = "[Boolean visualization output setting]"
-        if "commands" not in args.keys():
-            missing["commands"] ={
+        if "commands" not in args:
+            missing["commands"] = {
                 "serverStart": "[CLI command to start server]",
                 "runTests": "[CLI command to run test suite]"
             }
         else:
             if "serverStart" not in args["commands"]:
-                missing["commands"] ={
+                missing["commands"] = {
                     "serverStart": "[CLI command to start server]"
                 }
             if "runTests" not in args["commands"]:
-                if "commands" not in missing.keys():
-                    missing["commands"] ={
+                if "commands" not in missing:
+                    missing["commands"] = {
                         "runTests": "[CLI command to run test suite]"
                     }
                 else:
                     missing["commands"]["runTests"] = "[CLI command to run test suite]"
-        
+
         if len(missing) > 0:
-            raise InvalidConfig("Missing configuration fields: \n " + json.dumps(missing))
+            raise InvalidConfig(
+                "Missing configuration fields: \n " + json.dumps(missing))
 
     @staticmethod
     def validate_config_path(conf_path: str) -> None:
-        if not os.path.exists(conf_path):
-            raise NodewattsError("Configuration file does not exist at provided path")
+        if not os.path.exists(os.path.join(os.getcwd(), conf_path)):
+            raise NodewattsError(
+                "Configuration file does not exist at provided path")
 
     def _to_engine_format(self, args: dict) -> dict:
         parsed = {}
-        if "address" in args["database"].keys():
-            parsed["internal_db_addr"] = args["database"]["address"]
-        if "port" in args["database"].keys():
-            parsed["internal_db_port"] = args["database"]["port"]
-        if "exportRawData" in args["database"].keys():
-            parsed["export_raw"] = args["database"]["exportRawData"]
-        if "exportAddress" in args["database"].keys():
-            parsed["out_db_addr"] = args["database"]["exportAddress"]
-        if "exportPort" in args["database"].keys():
-            parsed["out_db_port"] = args["database"]["exportPort"]
-        if "verbose" in args.keys():
-            parsed["verbose"] = args["verbose"]
+        parsed["internal_db_uri"] = args["database"]["uri"]
+        parsed["export_raw"] = args["database"]["exportRawData"]
+        parsed["out_db_uri"] = args["database"]["exportUri"]
+        parsed["verbose"] = args["verbose"]
+        parsed["out_db_name"] = args["database"]["exportDbName"]
         return parsed
 
     def _generate_engine_conf(self, args: dict) -> Config:
         return super().__init__(args)
+
+    # This will fail unless both module config files are validated beforehand
+    def inject_module_config_vars(self):
+        with open(os.path.join(
+            os.getcwd(), "./nodewatts/config/hwpc_config.json"), "r+"
+        ) as f:
+            sensor = json.load(f)
+            sensor["verbose"] = self.verbose
+            sensor["output"]["uri"] = self.engine_conf_args["internal_db_uri"]
+            f.seek(0)
+            json.dump(sensor, f)
+            f.truncate()
+
+        with open(os.path.join(
+            os.getcwd(), "./nodewatts/config/smartwatts_config.json"), "r+"
+        ) as f:
+            smartwatts = json.load(f)
+            smartwatts["verbose"] = self.verbose
+            smartwatts["input"]["puller"]["uri"] = self.engine_conf_args["internal_db_uri"]
+            smartwatts["output"]["pusher_power"]["uri"] = self.engine_conf_args["internal_db_uri"]
+            f.seek(0)
+            json.dump(smartwatts, f)
+            f.truncate()
+
+    @staticmethod
+    def validate_sensor_config(args: dict) -> None:
+        sensor_schema = {
+            "type": "object",
+            "required": ["name", "verbose", "frequency", "output", "system", "container"],
+            "properties": {
+                "name": {"type": "string"},
+                "verbose": {"type": "boolean"},
+                "frequency": {"type": "number"},
+                "output": {
+                    "type": "object",
+                    "required": ["type", "uri", "database", "collection"],
+                    "properties": {
+                        "type": {"type": "string"},
+                        "uri": {"type": "string"},
+                        "database": {"type": "string"},
+                        "collection": {"type": "string"}
+                    }
+
+                },
+                "system": {
+                    "type": "object",
+                    "required": ["rapl", "msr"],
+                    "properties": {
+                        "rapl": {
+                            "type": "object",
+                            "required": ["events", "monitoring_type"],
+                            "properties": {
+                                "events": {"type": "array", "items": {"type": "string"}},
+                                "monitoring_type": {"type": "string"}
+                            },
+                        },
+                        "msr": {
+                            "type": "object",
+                            "required": ["events"],
+                            "properties": {
+                                "events": {"type": "array", "items": {"type": "string"}}
+                            }
+                        }
+                    }
+                },
+                "container": {
+                    "type": "object",
+                    "required": ["core"],
+                    "properties": {
+                        "core": {
+                            "type": "object",
+                            "required": ["events"],
+                            "properties": {
+                                "events": {"type": "array", "items": {"type": "string"}}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        try:
+            jschema.validate(instance=args, schema=sensor_schema)
+        except Exception as e:
+            raise InvalidConfig(
+                "Sensor configuration file is invalid: \n\n" + str(e)) from None
+
+    @staticmethod
+    def validate_smartwatts_config(args: dict) -> None:
+        sw_schema = {
+            "type": "object",
+            "required": ["verbose", "stream", "input", "output", "cpu-frequency-base", "cpu-frequency-min", "cpu-frequency-max", "cpu-error-threshold",
+                         "disable-dram-formula", "sensor-report-sampling-interval"],
+            "properties": {
+                "verbose": {"type": "boolean"},
+                "stream": {"type": "boolean"},
+                "input": {
+                    "type": "object",
+                    "required": ["puller"],
+                    "properties": {
+                        "puller": {
+                            "type": "object",
+                            "required": ["model", "type", "uri", "db", "collection"],
+                            "properties": {
+                                "model": {"type": "string"},
+                                "type": {"type": "string"},
+                                "uri": {"type": "string"},
+                                "db": {"type": "string"},
+                                "collection": {"type": "string"}
+                            }
+                        }
+                    }
+
+                },
+                "output": {
+                    "type": "object",
+                    "required": ["pusher_power"],
+                    "properties": {
+                        "pusher_power": {
+                            "type": "object",
+                            "required": ["type", "uri", "db", "collection"],
+                            "properties": {
+                                "type": {"type": "string"},
+                                "uri": {"type": "string"},
+                                "db": {"type": "string"},
+                                "collection": {"type": "string"}
+                            }
+                        }
+                    }
+
+                },
+                "cpu-frequency-base": {"type": "integer"},
+                "cpu-frequency-min": {"type": "integer"},
+                "cpu-frequency-max": {"type": "integer"},
+                "cpu-error-threshold": {"type": "number"},
+                "disable-dram-formula": {"type": "boolean"},
+                "sensor-report-sampling-interval": {"type": "integer"}
+            }
+        }
+
+        try:
+            jschema.validate(instance=args, schema=sw_schema)
+        except Exception as e:
+            raise InvalidConfig(
+                "Smartwatts configuration file is invalid: \n Please ensure you \
+                     have run the provided install.sh file. \n\n" + str(e)) from None
